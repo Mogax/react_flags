6db5fdfd64c2380cb034b8899ce0d31ec21dccca
owenm <owenmoga1208@gmail.com>
2021-10-29 15:31:59 +0200

    set Navigation.js set pages set scss set logo

---
+++ src/pages/About.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+    return (
+        <div>
+            <Navigation/>
+            <Logo/>
+            <h1>À propos</h1>
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias aperiam at, consectetur
+                cumque deleniti dolorem ea eaque eligendi esse facere fugit hic illum ipsam itaque iure magni molestiae
+                odit perspiciatis porro quia, quo quod, ratione recusandae rem reprehenderit rerum saepe similique sint
+                tempore? Cumque ducimus in magnam repellendus reprehenderit tempora vel voluptates. Atque doloremque
+                dolores labore sapiente. Ab accusamus aut commodi dolor dolorem, doloribus dolorum ea illo ipsum
+                mollitia nam numquam obcaecati optio perspiciatis quaerat, rem reprehenderit sapiente tempora tempore
+                tenetur ullam, voluptates voluptatibus. Ab aspernatur beatae consequuntur corporis distinctio dolor
+                doloremque eligendi eum eveniet ex, harum impedit, in, incidunt itaque iure laboriosam libero magnam
+                minus nostrum optio placeat provident ratione recusandae rem repellat reprehenderit similique velit
+                veritatis voluptatem voluptatum! Accusamus adipisci atque blanditiis consectetur consequatur ducimus
+                earum enim est, exercitationem illo impedit ipsam laborum laudantium nisi officia quaerat quas quibusdam
+                quidem quis quod repellat repellendus, sed tempora temporibus vero vitae voluptatum. Ab asperiores
+                consectetur consequatur dolorum enim error esse excepturi fugit illum, inventore magni odio quibusdam
+                quidem quo repellat repellendus reprehenderit soluta, tempora velit voluptatum. Accusamus, assumenda
+                atque aut cumque dolorem eaque eveniet, fugiat in magni natus nostrum pariatur perspiciatis quam
+                repellendus reprehenderit voluptas voluptatibus! Consectetur dolorem doloribus laudantium tempora.
+            </div>
+            <br/>
+            <div>A animi aspernatur consectetur consequatur consequuntur corporis cum cupiditate ducimus earum esse est
+                eveniet ex expedita hic impedit, itaque laboriosam laborum libero maxime molestias nisi, perspiciatis
+                porro quam qui quidem tempora tempore ullam velit, voluptate voluptates. Amet fuga fugiat nam quas
+                quisquam, saepe similique. Accusamus alias aliquam cum cumque deleniti dicta dignissimos ea eum ex
+                excepturi expedita fuga id ipsa laborum laudantium magnam minus molestias nam nemo neque officia,
+                provident quasi quia ratione repellat repudiandae similique sint unde ut voluptatibus? Adipisci commodi
+                enim et expedita fugiat, illum inventore, maxime mollitia nulla pariatur placeat quaerat sunt ullam
+                veritatis voluptatum? Accusantium ad earum eos id. A aspernatur doloremque est facilis obcaecati,
+                officia praesentium sequi voluptas. A amet asperiores, autem cupiditate deserunt dolore dolorum ducimus
+                explicabo mollitia similique? A accusantium architecto assumenda beatae, culpa deserunt dolor explicabo
+                facere harum impedit in labore maxime quaerat quisquam quo quod, reiciendis, sequi suscipit! Adipisci
+                animi consectetur cum dolores ducimus, illo minus perspiciatis quidem quis recusandae? Distinctio ipsa
+                nemo qui suscipit totam ut? At commodi consequuntur, doloribus inventore itaque mollitia tempora! Amet
+                aperiam debitis facere illo quia. Cum dicta doloribus, earum in ipsa magnam maiores sint tempora veniam
+                voluptatem! Eaque magni neque quas tenetur ullam, veritatis voluptate. Aliquid, in.
+            </div>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const Home = () => {
+    return (
+        <div className="home">
+            <Navigation/>
+            <Logo/>
+            <h1>Accueil</h1>
+        </div>
+    );
+};
+
+export default Home;
